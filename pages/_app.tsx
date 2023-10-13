fb72a4d1bdfaaaacc5bd53b12889f2b9f3368422
d0wnlore <81343175+d0wnlore@users.noreply.github.com>
2023-10-13 15:52:24 +0900

    Configuer RainbowKit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,32 +4,23 @@ import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  base,
-  zora,
+  scrollSepolia,
+  mantleTestnet,
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    scrollSepolia,
+    mantleTestnet,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [scrollSepolia] : []),
   ],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'Blackpool',
+  projectId: '06ee5a56ce0a60a01c48405a64a08c12',
   chains,
 });
 
