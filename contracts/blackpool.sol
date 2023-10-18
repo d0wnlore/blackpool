da2573bbdb0e38f17fd2ce89946d5156d2d197da
d0wnlore <81343175+d0wnlore@users.noreply.github.com>
2023-10-18 15:59:08 +0900

    Modify contract

--- contracts/blackpool.sol
+++ contracts/blackpool.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 contract Blackpool {
     address public owner;
@@ -8,7 +9,13 @@ contract Blackpool {
         bool exists;
     }
 
+    struct SiteInfo {
+        string url;
+        address owner;
+    }
+
     mapping(address => Site[]) public addrSites;
+    SiteInfo[] public allSites;
 
     constructor() {
         owner = msg.sender;
@@ -16,6 +23,7 @@ contract Blackpool {
 
     function addSite(string calldata url) public {
         addrSites[msg.sender].push(Site(url, true));
+        allSites.push(SiteInfo(url, msg.sender));
     }
 
     function getSite(address addr, uint index) public view returns (string memory) {
@@ -27,7 +35,19 @@ contract Blackpool {
         return addrSites[msg.sender].length;
     }
 
-    function getAllSites(address addr) public view returns (string[] memory) {
+    function getAllSites() public view returns (string[] memory) {
+        uint totalSitesCount = allSites.length;
+
+        string[] memory allUrls = new string[](totalSitesCount);
+
+        for (uint i = 0; i < totalSitesCount; i++) {
+            allUrls[i] = allSites[i].url;
+        }
+
+        return allUrls;
+    }
+
+    function getAllAddrSites(address addr) public view returns (string[] memory) {
         uint len = addrSites[addr].length;
         string[] memory urls = new string[](len);
 
