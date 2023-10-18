f3a5ef229c764d6a73eb0fb9dc4426691df6c033
d0wnlore <81343175+d0wnlore@users.noreply.github.com>
2023-10-18 16:56:03 +0900

    Fetch list from Mantle

--- pages/list.tsx
+++ pages/list.tsx
@@ -1,12 +1,20 @@
-import { useContractRead, useNetwork } from 'wagmi';
+import { useContractRead } from 'wagmi';
+import { getNetwork } from '@wagmi/core';
 import { useState } from 'react';
 
+const { chain, chains } = getNetwork();
+
+const CONTRACT_ADDRESSES = {
+  534351: '0xcE5a0B27f89FdaB2B1f1809A3560fF82C582f596',
+  5001: '0xD30e9367171CB05aC5E00ae195A015225F2848e1'
+}
+
 function List() {
-  const { chain } = useNetwork();
+  const { chain } = getNetwork();
   const [siteList, setSiteList] = useState(null);
 
   useContractRead({
-    address: '0xcE5a0B27f89FdaB2B1f1809A3560fF82C582f596',
+    address: CONTRACT_ADDRESSES[chain.id] as `0x${string}`,
     abi: [{
       "inputs": [],
       "name": "getAllSites",
@@ -29,7 +37,7 @@ function List() {
 
   return (
     <div>
-      <p suppressHydrationWarning>{chain && chain.name}</p>
+      <p suppressHydrationWarning>{chain && chain.id}</p>
       <p>{siteList}</p>
     </div>
   )
